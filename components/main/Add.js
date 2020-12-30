9413b6991e95d214ca23c285f2f0e549bd8b564c
Shashank <s_shashank@outlook.com>
2020-12-30 23:37:55 +0530

    implement camera and image gallery

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,12 +1,129 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+  Image,
+  Platform,
+} from "react-native";
+import { Camera } from "expo-camera";
+import * as ImagePicker from "expo-image-picker";
 
 const Add = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const getRatios = async () => {
+    if (camera) {
+      let ratios = await camera.getSupportedRatiosAsync();
+      console.log(ratios);
+      console.log("ratio");
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+
+    (async () => {
+      if (Platform.OS !== "web") {
+        const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (galleryStatus.status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
+    }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View>
-      <Text>Add Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          style={styles.camera}
+          type={type}
+          ref={(ref) => setCamera(ref)}
+          ratio={"3:4"}
+        />
+      </View>
+
+      <Button
+        title="Flip Image"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      ></Button>
+      <Button title="Take Picture" onPress={() => takePicture()}></Button>
+      <Button title="Import From Gallery" onPress={() => pickImage()}></Button>
+      {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cameraContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  camera: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+});
 
 export default Add;
